ae76491e339977aa519e31c671e48310b271c474
ArthurASB <195683488+ArthurASB@users.noreply.github.com>
2025-05-05 00:07:39 -0300

    Fix operator calculating without second operand

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ kb = document.querySelector(".keyboard");
 numbers = document.querySelectorAll(".numerals");
 opps = document.querySelectorAll(".operators");
 clearScreen = kb.querySelector("#clear");
+equalSign = kb.querySelector("#equal");
 
 
 display.textContent = "0";
@@ -51,16 +52,15 @@ clearScreen.addEventListener("click", () => {
     fstOperand = 0;
     sndOperand = 0;
     opertr = 0;
-    oprToggle = 0;
+    opertrToggled = 0;
 })
 
-//varrendo o NodeList "buttons" para poder acessá-los individualmente
 numbers.forEach(numbtn => {
     numbtn.addEventListener("click", () => {
         if (display.textContent == "0") display.textContent = "";
-        if (fstOperand !== 0 && oprToggle == 0) {
+        if (fstOperand !== 0 && opertrToggled == 0) {
             display.textContent = "";
-            oprToggle = 1;
+            opertrToggled = 1;
         }
         if (display.textContent.length <= 12) {            
             display.textContent += numbtn.textContent;
@@ -70,16 +70,32 @@ numbers.forEach(numbtn => {
 
 opps.forEach(operator => {
     operator.addEventListener("click", () => {
+        //first operand has been typed
         if (display.textContent !== "0" && fstOperand == 0) {
             fstOperand = Number(display.textContent);
             opertr = operator.textContent;
-        } else if (fstOperand !== 0){
+        //second operand has been typed or user wants to operate 
+        //with equal first and second operands    
+        } else if (fstOperand !== 0 && opertrToggled === 1){            
             sndOperand = Number(display.textContent);
             let result = operate(fstOperand, opertr, sndOperand);
             display.textContent = "";
             display.textContent = result;
             fstOperand = result;
-            oprToggle = 0;          
-        }        
+            opertrToggled = 0;          
+        }
     })
 })
+
+equalSign.addEventListener("click", () => {
+    if (opertr!=0 && opertrToggled == 1){
+        if (opertr!=0){
+        sndOperand = Number(display.textContent);
+        let result = operate(fstOperand, opertr, sndOperand);
+        display.textContent = "";
+        display.textContent = result;
+        fstOperand = result;
+        opertrToggled = 0;  
+    }
+}}
+)
